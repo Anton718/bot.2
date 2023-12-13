30cf819eab17e6afd54e79860b252cf37854f54b
Anton <112634784+Anton718@users.noreply.github.com>
2023-12-13 10:07:12 -0500

    Update appendUser.js

--- controllers/appendUser.js
+++ controllers/appendUser.js
@@ -1,31 +1,34 @@
 const fs = require("fs");
 
 exports.addUser = (ctx) => {
-    const from = ctx.update.message.from;
-    new_user = {
-      id: from.id,
-      username: from.username,
-      name_: from.first_name,
-      isCountry: false,
-      isAge: false,
-      isBuilt: false,
-      isTested: false,
-      isCandidate: false,
-    };
-    let data;
-    let obj = fs.readFileSync("./assets/users.json", 'utf-8')
-    console.log("obj:", obj);
-    if (obj) {
-        data = JSON.parse(obj);
-        for (let i of data.users) {
-            i.id === new_user.id ? null : data.users.push(new_user);
-        }
-    } else {
-        data = {
-        users: [],
-        };
-        data.users.push(new_user)
+  const from = ctx.update.message.from;
+  new_user = {
+    id: from.id,
+    username: from.username,
+    name_: from.first_name,
+    isCountry: false,
+    isAge: false,
+    isBuilt: false,
+    isTested: false,
+    isCandidate: false,
+  };
+  let data;
+  let obj = fs.readFileSync("./assets/users.json", "utf-8");
+  if (obj) {
+    data = JSON.parse(obj);
+    const temp = [];
+    for (let i of data.users) {
+      temp.push(i.id);
     }
-    fs.writeFileSync("./assets/users.json", JSON.stringify(data));
-    ctx.sendMessage(`Hello, ${new_user.name_}, how are you ??`)
-  }
+    if (!temp.includes(new_user.id)) {
+      data.users.push(new_user);
+    }
+        } else {
+            data = {
+            users: [],
+            };
+            data.users.push(new_user);
+        }
+  fs.writeFileSync("./assets/users.json", JSON.stringify(data));
+  ctx.sendMessage(`Hello, ${new_user.name_}, how are you ??`);
+};
